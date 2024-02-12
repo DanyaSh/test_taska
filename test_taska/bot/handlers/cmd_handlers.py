99ccbe02aed917ab47d916737c5ed93d8184e6be
DanyaSh <danilashundeev@gmail.com>
2024-02-12 18:44:52 -0300

    change ikb, add help, and move texts

--- test_taska/bot/handlers/cmd_handlers.py
+++ test_taska/bot/handlers/cmd_handlers.py
@@ -27,12 +27,20 @@ from aiogram.types import Message, ReplyKeyboardRemove
 import bot.keyboards.user_keyboards as ikb
 import bot.texts.user_texts as txt
 
-router = Router()  # [1]
+router = Router()
 
-@router.message(Command("start"))  # [2]
+@router.message(Command("start"))
 async def cmd_start(msg: Message):
     reply_text=txt.start.format(name=msg.from_user.first_name)
     await msg.answer(
         text=reply_text,
         reply_markup=ikb.get_start_ikb()
+    )
+
+@router.message(Command("help"))
+async def cmd_help(msg: Message):
+    reply_text=txt.help
+    await msg.answer(
+        text=reply_text,
+        reply_markup=ikb.get_home_ikb()
     )
